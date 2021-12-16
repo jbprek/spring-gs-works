d79d1d2f9d78a7f9b4e24b351193f06e4126991d
Ioannis Prekezes <jbprek@gmail.com>
2021-12-16 06:52:09 +0000

    WIP

--- gs-spring-ws/Readme.md
+++ gs-spring-ws/Readme.md
@@ -9,7 +9,7 @@ slightly adapted from :https://spring.io/guides/gs/consuming-web-service/
 ##gs-ws-consumer-aug :
 ##gs-ws-consumer-aug : 
 The above example improved to:
-1. coonsumer - compile without the producer running
+1. consumer - compile without the producer running
 2. Log requests/responses through logger config
 3. Externalize in configuration Producer URL 
-4. Improper xsd in client: meta field in Request defined as sting when in practice is a complex type (IP, datetime), xsd definition missing in client
+4. Log requests/responses on demand
--- gs-spring-ws/gs-ws-consumer-aug/src/main/java/com/example/country/ws/client/CountryClient.java
+++ gs-spring-ws/gs-ws-consumer-aug/src/main/java/com/example/country/ws/client/CountryClient.java
@@ -3,22 +3,41 @@ package com.example.country.ws.client;
 
 import com.example.country.ws.dto.GetCountryRequest;
 import com.example.country.ws.dto.GetCountryResponse;
-
-import java.io.StringReader;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-
+import com.example.country.ws.dto.MetaInfo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.stereotype.Component;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+
+import javax.xml.transform.Result;
+import javax.xml.transform.stream.StreamResult;
+import java.io.IOException;
+import java.io.StringWriter;
 
 public class CountryClient extends WebServiceGatewaySupport {
 
 	private static final Logger log = LoggerFactory.getLogger(CountryClient.class);
 
-	public GetCountryResponse getCountry(String country) {
+	@Autowired
+	Jaxb2Marshaller jaxb2Marshaller;
+
+
+	public GetCountryResponse getCountry(String country) throws IOException {
 
 		GetCountryRequest request = new GetCountryRequest();
+
+		jaxb2Marshaller.
+
+
+
+//		MetaInfo metaInfo = new MetaInfo();
+//		StringWriter sw = new StringWriter();
+//
+//		Result result = new StreamResult(sw);
+//		getMarshaller().marshal(metaInfo,result);
+//		String metaInfoXml = sw.toString();
 		request.setName(country);
 
 		log.info("Requesting location for " + country);
